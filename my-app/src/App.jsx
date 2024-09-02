18b31e7a0c4b05e080cbf7f2c402896e1ffafe13
Sam Ly <sam@samly.me>
2024-09-02 12:57:14 -0700

    fix formatting

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,36 +6,36 @@ import { rmCountry } from "./services/countryService";
 
 
 const countryList = () => {
-  const {countries, loading, error} = useCountries();
+  const { countries, loading, error } = useCountries();
 
 
-if (loading) { 
-  return <p>loading</p>
-}
+  if (loading) {
+    return <p>loading</p>
+  }
 
-if (error) { 
-  return <p>error</p>
-}
+  if (error) {
+    return <p>error</p>
+  }
   return (
     <div>
-    <button onClick={addCountry}>add to db</button>
-    {/* <form>
+      <button onClick={addCountry}>add to db</button>
+      {/* <form>
       <input input type="text" id="uniqueId" value="value"></input>
     </form> */}
-    {/* <form id="animal">
+      {/* <form id="animal">
     <label for="animal">What is your favorite animal?</label>
     <input type="text" id="animal" name="country" value="dog"></input>
     <button>Save</button>
   </form> */}
 
-    <button onClick={rmCountry}>Remove from db</button> 
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
+      <button onClick={rmCountry}>Remove from db</button>
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name}>{country.name}</li>
+        ))}
+      </ul>
     </div>
-    
+
   );
 }
 
