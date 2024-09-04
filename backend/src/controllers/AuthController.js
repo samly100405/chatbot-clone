efa9b1af6d020c6f81fc722866f3ee66c8428375
Sam Ly <sam@samly.me>
2024-09-03 22:27:58 -0700

    sorta working, pre save hook for user not working

---
+++ backend/src/controllers/AuthController.js
@@ -0,0 +1,36 @@
+import User from '../models/UserModel.js'
+import { createSecretToken } from '../util/SecretToken.js'
+import bcrypt from 'bcrypt'
+
+export async function Signup(req, res, next) {
+    try {
+        const { email, password, username, createdAt } = req.body
+
+        if (await User.findOne({ email })) {
+            return res
+                    .status(409)
+                    .json({ message: 'User already exists'})
+        }
+
+        const user = await User.create({ email, password, username, createdAt})
+        const token = createSecretToken(user._id)
+
+        res.cookie('token', token, {
+            withCredential: true,
+            httpOnly: false,
+        })
+
+        res
+            .status(201)
+            .json({
+                message: "User signed up successfulyl",
+                success: true,
+                user,
+            })
+        
+        next()
+
+    } catch (error) {
+        console.error(error)
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,34 @@
 import express from 'express'
-import chatRouter from './routes/chat.js'
+import mongoose from 'mongoose'
+
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+import 'dotenv/config'
+
+import authRoute from './routes/AuthRoute.js'
+const { MONGODB_URL, PORT } = process.env
+
+mongoose
+  .connect(MONGODB_URL, {
+    autoIndex: false,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err))
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+        origin: [`http://localhost:${PORT}`],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true,
+    })
+)
 
-app.use('/chat', chatRouter)
-
-const port = 3000
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
 })
+
+app.use('/', authRoute)
