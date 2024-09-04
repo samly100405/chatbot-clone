a389717c9c9bc540933df8db2d604d892815e1bb
Sam Ly <sam@samly.me>
2024-09-03 21:27:57 -0700

    snapshot before shit hits the fan lmao

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import mongoose from 'mongoose'
 import { UserSchema, ChatHistorySchema } from './schemas.js'
 
-mongoose.connect(process.env.MONGODB_URI, { autoIndex: false })
+mongoose.connect(process.env.MONGODB_URL, { autoIndex: false })
 
 const User = mongoose.model('User', UserSchema)
 const ChatHistory = mongoose.model('ChatHistory', ChatHistorySchema)
