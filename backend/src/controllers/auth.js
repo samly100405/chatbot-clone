6bf562d29a960b3f17779bff75ab0c13d1b42232
Sam Ly <sam@samly.me>
2024-09-06 16:47:17 -0700

    implement user creation in auth controller

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,6 +1,8 @@
 import passport from 'passport'
 import GoogleStrategy from 'passport-google-oidc'
 
+import userModel from '../models/user.js'
+
 import 'dotenv/config'
 
 passport.use(
@@ -9,25 +11,43 @@ passport.use(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: `http://localhost:${process.env.SERVER_PORT}/oauth2/redirect/google`
     },
-    function verify(issuer, profile, cb) {
-        // TODO: make this function actually check
-        return cb(null, { id: profile.id })
-    }
-))
+        function verify(issuer, profile, cb) {
+            console.log(profile)
+            userModel.findOne({
+                oauthID: profile.id,
+            })
+                .then(
+                    (res) => {
+                        if (res) { return cb(null, res) }
+                        userModel.create({ oauthID: profile.id })
+                            .then(
+                                (res) => {
+                                    cb(null, res)
+                                }
+                            )
+                    }
+                )
+        }
+    )
+)
 
 passport.serializeUser(
     (user, cb) => {
-        process.nextTick(() => {
-            cb(null, { id: user.id  })
-        })
+        process.nextTick(
+            () => {
+                cb(null, { id: user.id })
+            }
+        )
     }
 )
 
 passport.deserializeUser(
     (user, cb) => {
-        process.nextTick(() => {
-            return cb(null, user)
-        })
+        process.nextTick(
+            () => {
+                return cb(null, user)
+            }
+        )
     }
 )
 
