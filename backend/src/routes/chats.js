f02baeaf938ae82ec347363de8fa27fa33e7f6b9
Sam Ly <sam@samly.me>
2024-09-06 00:28:40 -0700

    extract chats routes

--- backend/src/routes/chats.js
+++ backend/src/routes/chats.js
@@ -4,8 +4,7 @@ const router = express.Router()
 
 router.get('/', 
     (req, res, next) => {
-        if (!req.user) res.render('index')
-        else res.redirect('/chats')
+        res.render('chats')
     }
 )
 
