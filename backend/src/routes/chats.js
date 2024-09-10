ff70d6c476486424c11f9a7db92c475a26847d95
Sam Ly <sam@samly.me>
2024-09-09 23:54:12 -0700

    working ig

--- backend/src/routes/chats.js
+++ backend/src/routes/chats.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { createChat, getChats } from '../controllers/chats'
+import { createChat, getChats } from '../controllers/chats.js'
 
 const router = express.Router()
 
