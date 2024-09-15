4a02b8bcc2b7039cda47206f2c56eca6a1a45a52
Sam Ly <sam@samly.me>
2024-09-15 11:40:47 -0700

    fix format

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [countries, setcountries] = useState()
 
   useEffect(() => {
-    async function grabData() { 
+    async function grabData() {
       let data = await getCountries()
       if (data) {
         setcountries(data)
@@ -19,69 +19,66 @@ function App() {
 
   return (
     <>
-
-
-
-<body>
-<nav id="sidebar">
-        <div class="float-top">
+      <body>
+        <nav id="sidebar">
+          <div class="float-top">
             <div class="sidebar-controls">
-                <button class="new-chat"><i class="fa fa-plus"></i> New chat</button>
-                <button class="hide-sidebar"><i class="fa fa-chevron-left"></i></button>
+              <button class="new-chat"><i class="fa fa-plus"></i> New chat</button>
+              <button class="hide-sidebar"><i class="fa fa-chevron-left"></i></button>
             </div>
             <ul class="conversations">
-                <li class="grouping">Today</li>
-                <li class="active">
-                    <button class="conversation-button"><i class="fa fa-message fa-regular"></i>
-                        
-                          <nav id ="sidebar">
-                            {countries?.map((country) => {
-                              return (
-                                <>
-                                  <h1>{country.name}</h1>
-                                </>
-                                )
-                            })}
-                        </nav>
-                    </button>
-                    <div class="fade"></div>
-                    <div class="edit-buttons">
-                        <button><i class="fa fa-edit"></i></button>
-                        <button><i class="fa fa-trash"></i></button>
-                    </div>
-                </li>
+              <li class="grouping">Today</li>
+              <li class="active">
+                <button class="conversation-button"><i class="fa fa-message fa-regular"></i>
+
+                  <nav id="sidebar">
+                    {countries?.map((country) => {
+                      return (
+                        <>
+                          <h1>{country.name}</h1>
+                        </>
+                      )
+                    })}
+                  </nav>
+                </button>
+                <div class="fade"></div>
+                <div class="edit-buttons">
+                  <button><i class="fa fa-edit"></i></button>
+                  <button><i class="fa fa-trash"></i></button>
+                </div>
+              </li>
             </ul>
-        </div>
-        <div class="user-menu">
+          </div>
+          <div class="user-menu">
             <button>
-                <i class="user-icon">u</i>
-                username
-                <i class="fa fa-ellipsis dots"></i>
+              <i class="user-icon">u</i>
+              username
+              <i class="fa fa-ellipsis dots"></i>
             </button>
             <ul>
-                <li><button>My plan</button></li>
-                <li><button>Custom instructions</button></li>
-                <li><button>Settings &amp; Beta</button></li>
-                <li><button>Log out</button></li>
+              <li><button>My plan</button></li>
+              <li><button>Custom instructions</button></li>
+              <li><button>Settings &amp; Beta</button></li>
+              <li><button>Log out</button></li>
             </ul>
-        </div>
-    </nav>
-    <main>
+          </div>
+        </nav>
+        <main>
 
 
-            <div class="logo">
-               Chatbot Clone
-            </div>
+          <div class="logo">
+            Chatbot Clone
+          </div>
 
-        <div id="message-form">
+          <div id="message-form">
             <div class="message-wrapper">
-                <textarea id="message" rows="1" placeholder="Send a message"></textarea>
-                <button class="send-button"><i class="fa fa-paper-plane"></i></button>
+              <textarea id="message" rows="1" placeholder="Send a message"></textarea>
+              <button class="send-button"><i class="fa fa-paper-plane"></i></button>
             </div>
-        </div>
-    </main>
+          </div>
+        </main>
 
-    </body>
+      </body>
     </>
   )
 }
