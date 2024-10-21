612ebffb01a7ee7a36bb714ae6f6931e5703f461
Sam Ly <sam@samly.me>
2024-10-21 15:55:04 -0700

    working crud, no chat yet

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,62 +1,47 @@
-import { useContext, useEffect, useState } from 'react'
-import chatService from '../services/chats.js'
-import { ChatContext } from '../contexts.js'
+import { useState } from "react"
+import { Link } from "react-router-dom"
 
-function Sidebar({ setSelectedChat }) {
-  const [chats, setChats] = useState([])
+export default function Sidebar({ chats }) {
+    const [selected, setSelected] = useState(chats[0]._id)
 
-  const chatContext = useContext(ChatContext)
-
-  useEffect(() => {
-    // TODO: implement user id context
-    chatService.getChats(1)
-      .then(
-        (res) => {
-          if (res) setChats(res)
-          setSelectedChat(res[0])
-        }
-      )
-
-  }, [])
-
-  return (
-    <div className="sidebar">
-      {
-        chats.map((elem) => {
-          return <SidebarItem text={elem.name}
-            setSelected={() => setSelectedChat(elem)}
-            selected={elem.id === chatContext.id}
-            key={elem.id} />
-        })
-      }
-      <NewChatButton />
-    </div>
-  )
+    return (
+        <div className="sidebar">
+            {
+                chats.map(
+                    elem => {
+                        return <SidebarItem
+                            text={elem.name}
+                            setSelected={() => setSelected(elem._id)}
+                            selected={elem._id === selected}
+                            id={elem._id}
+                            key={elem._id}
+                        />
+                    }
+                )
+            }
+            <NewChatButton />
+        </div>
+    )
 }
 
-function SidebarItem({ text, setSelected, selected }) {
-  // TODO: Implement delete
-  return (
-    <div className={'sidebar-item ' + (selected && 'selected')} onClick={setSelected}>
-      {text}
-      <button>del</button>
-    </div>
-  )
+function SidebarItem({ text, setSelected, selected, id }) {
+    // TODO: Implement delete
+    return (
+        <Link to={`chat/${id}`}>
+            <div className={'sidebar-item ' + (selected && 'selected')} onClick={setSelected}>
+                {text}
+                <button>del</button>
+            </div>
+        </Link>
+    )
 }
 
-// make new chat a form
 function NewChatButton({}) {
-  return (
-    <div className="sidebar-item new-chat">
-      <button onClick={
-        (event) => {
-          chatService.createChat(1, "ay lmao")
-        }
-      }>
-        new chat +
-      </button>
-    </div>
-  )
+    return (
+        <Link to="/new-chat">
+            <div className="sidebar-item new-chat">
+                <button>new chat +</button>
+            </div>
+        </Link>
+    )
 }
-
-export default Sidebar
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,41 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+import { createBrowserRouter, RouterProvider, useParams } from 'react-router-dom'
+
 import './index.css'
+import App from './App'
+import ErrorPage from './pages/ErrorPage'
+import ChatPage from './pages/ChatPage'
+import NewChatPage from './pages/NewChatPage'
+
+import { loader as chatLoader } from './pages/ChatPage'
+import { loader as chatsLoader } from './App'
+import { action as newChatAction } from './pages/NewChatPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    loader: chatsLoader,
+    children: [
+      {
+        path: 'new-chat/',
+        element: <NewChatPage />,
+        action: newChatAction,
+      },
+      {
+        path: 'chat/:chatID',
+        element: <ChatPage />,
+        loader: chatLoader,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
